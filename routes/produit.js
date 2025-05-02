111698c3e9dd2702132f09c4d304b58a4bb22027
oumy25 <kaneoumylaye@gmail.com>
2025-05-02 20:05:57 +0000

    enlever la vérification du token au niveau de tous les produits et produit par id

--- routes/produit.js
+++ routes/produit.js
@@ -3,8 +3,8 @@ const router = express.Router()
 const produitsController = require("../controllers/produitController")
 const verifyToken = require("../middleware/verifyToken");
 
-router.get('/', verifyToken, produitsController.getAllProduits );
-router.get('/:id', verifyToken, produitsController.getProduitById );
+router.get('/', produitsController.getAllProduits );
+router.get('/:id', produitsController.getProduitById );
 router.post('/add', verifyToken, produitsController.createProduit );
 router.put('/edit/:id', verifyToken, produitsController.updateProduit );
 router.delete('/delete/:id', verifyToken, produitsController.deleteProduit );
