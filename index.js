88a64952090b27e8eda4f8450310d2f87efbca31
oumy25 <kaneoumylaye@gmail.com>
2025-05-03 05:26:14 +0000

    ajout dossier static pour les images

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require ( 'express' );
+const path = require('path');
 const cors = require('cors'); 
 const app = express ();
 const connectDB = require("./db");
@@ -16,6 +17,7 @@ app.use(cors({
   }));
 app.use (express.json ());
 app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api/users', usersRoutes)
 app.use("/api/auth", authRoutes)
