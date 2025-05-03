817b71e80df7f1f278d974140965dcf68140775e
oumy25 <kaneoumylaye@gmail.com>
2025-05-03 05:54:31 +0000

    ajout dossier static pour les images

---
+++ middleware/upload.js
@@ -0,0 +1,17 @@
+const multer = require('multer');
+const path = require('path');
+
+// Configuration du stockage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // dossier où les images seront stockées
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + '-' + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
--- routes/produit.js
+++ routes/produit.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const router = express.Router()
 const produitsController = require("../controllers/produitController")
+const upload = require('../middlewares/upload'); // importe le middleware
+const Produit = require('../models/produit');
 const verifyToken = require("../middleware/verifyToken");
 
 router.get('/', produitsController.getAllProduits );
@@ -8,5 +10,25 @@ router.get('/:id', produitsController.getProduitById );
 router.post('/add', verifyToken, produitsController.createProduit );
 router.put('/edit/:id', verifyToken, produitsController.updateProduit );
 router.delete('/delete/:id', verifyToken, produitsController.deleteProduit );
+
+router.post('/add', upload.single('image'), async (req, res) => {
+    try {
+      const { title, prix, description } = req.body;
+      const image = req.file.filename;
+  
+      const produit = new Produit({
+        title,
+        prix,
+        description,
+        image,
+        userId: req.user.id, // si tu gères l’auth
+      });
+  
+      await produit.save();
+      res.status(201).json(produit);
+    } catch (error) {
+      res.status(500).json({ message: 'Erreur serveur', error });
+    }
+  });
 
 module.exports = router;
