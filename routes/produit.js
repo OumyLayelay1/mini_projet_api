177f05ff10391d5aae89f0751a6df14c99885dcd
oumy25 <kaneoumylaye@gmail.com>
2025-05-03 08:13:31 +0000

    dossier uploads ajouté

--- routes/produit.js
+++ routes/produit.js
@@ -8,6 +8,7 @@ router.get('/', produitsController.getAllProduits );
 router.post('/add', verifyToken, upload.single('image'), produitsController.createProduit );
 router.put('/edit/:id', verifyToken, produitsController.updateProduit );
 router.delete('/delete/:id', verifyToken, produitsController.deleteProduit );
+
 router.get('/:id', produitsController.getProduitById );
 
 
